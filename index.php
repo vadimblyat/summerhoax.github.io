7c4564fabb16690446358a9c96268edecbd28718
hb4u homebrew <38268052+hb4uhomebrew@users.noreply.github.com>
2019-08-24 20:07:38 -0400

    Add MCFreeHosting LTD
    This host has screwed over another host massively & scammed my friend. Proof below.
    https://cdn.discordapp.com/attachments/598253467840282664/614971738488569865/image0.png
    https://cdn.discordapp.com/attachments/598253467840282664/614971739012726784/image1.png
    https://cdn.discordapp.com/attachments/598253467840282664/614971739012726785/image2.png
    https://cdn.discordapp.com/attachments/598253467840282664/614971829966340096/image0.png
    https://cdn.discordapp.com/attachments/598253467840282664/614972420616487008/image0.png
    https://cdn.discordapp.com/attachments/598253467840282664/614972420650041524/image1.png
    https://cdn.discordapp.com/attachments/598253467840282664/614972421849743374/image3.png
    https://cdn.discordapp.com/attachments/598253467840282664/614972422327762955/image4.png (Him going away for 3 days randomly)
    https://i.imgur.com/vbyjJtC.png (Him sending my friends name & saying he suspended by VPS cause I'm friends with him.)
    https://i.imgur.com/xstKWlz.png (Racial Slurs claims that it was to make a point, showing racial slurs in your host's public discord is not a good thing.) 
    https://i.imgur.com/CyJp7OY.png *sighs* yet again, anti-jew jokes...
    
    
    Overall this host is a terrible, unsustainable & unprofessional waste of time.

--- index.php
+++ index.php
@@ -288,6 +288,10 @@
 			    <li data-placement="left" data-toggle="tooltip" title="VapeHost had many issues such as downtime, ignoring customers & of course using bad security practices and having a breach of all client data (Including Address's, Phone Numbers, Payment Info). VapeHost was one of the Lifetime Host's which advertise lifetime hosting but are really the lifetime of the company. He got hacked and then ghosted the whole discord for days and still not responded ever.">
                             <a href="https://vapehost.cc">VapeHost</a>
                         </li>
+			    </li>
+			    <li data-placement="left" data-toggle="tooltip" title="MCFreeHosting LTD sold a WHMCS key to a client & never got the key, then when the client asked for a refund he was denied, when he charged back he also suspended the clients friend's server which had clients on it. After that the owner MVP kept ignoring the friends messages about getting unsuspended for an hour to pull clients. They sell cheap hetzner dedicated server's using SolusVM and are not sustainable (Owner has said he spends around 1k a month on a free host) and will likely shutdown soon enough. They are legally registered but seem to have used the company for something else for selling tabacoo. Owner leaves for todays without responding to messages. Reinstalls panel every few days for some reason. Overall an unprofessional and scummy host.">
+                            <a href="https://redrock.host">MCFreeHosting LTD</a>
+                        </li>
 			    
                     </ul>
                     <p class="text-muted">We take no responsibility for the accuracy of these listings.</p>
